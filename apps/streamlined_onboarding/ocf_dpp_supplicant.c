3360f5f27bf0f21492dfdf0840978a9ce8e5208e
Andy Dolan <20274393+adolan5@users.noreply.github.com>
2021-07-02 10:31:26 -0600

    Create so_info and copy device UUID

--- apps/streamlined_onboarding/ocf_dpp_supplicant.c
+++ apps/streamlined_onboarding/ocf_dpp_supplicant.c
@@ -1,5 +1,19 @@
+#include "oc_api.h"
+#include "oc_core_res.h"
+#include "oc_pstat.h"
+#include "oc_streamlined_onboarding.h"
+
 int
 dpp_so_info_init(void)
 {
+  oc_sec_pstat_t *ps = oc_sec_get_pstat(0);
+  if (ps->s != OC_DOS_RFOTM) {
+    OC_DBG("Device not in RFOTM; will not generate SO info");
+    return 1;
+  }
+
+  oc_so_info_t so_info;
+  oc_uuid_to_str(oc_core_get_device_id(0), so_info.uuid, sizeof(so_info.uuid));
+  OC_DBG("Generating DPP streamlined onboarding info for with UUID: %s", so_info.uuid);
   return 0;
 }
